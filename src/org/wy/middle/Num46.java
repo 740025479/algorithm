077df245e554d70dde33738c6e9d7d06ca49bf63
fudan_wy <740025479@qq.com>
2019-10-28 15:05:22 +0800

    回溯思路

--- src/org/wy/middle/Num46.java
+++ src/org/wy/middle/Num46.java
@@ -41,5 +41,37 @@ public class Num46 {
         return res;
     }
 
+    /**
+     * 回溯算法
+     * 往往跟深搜一起，搜到一定深度后要返回之前的状态，就是回溯
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute1(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        List<Integer> list = new ArrayList<>();
+        dfs(res,visited,nums,list);
+        return res;
+    }
+
+    private void dfs(List<List<Integer>> res,boolean[] visited,int[] nums, List<Integer> cur){
+
+        if (cur.size()==nums.length){
+            res.add(cur);
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i])continue;
+            visited[i]=true;
+            List<Integer> temp = new ArrayList<>();
+            temp.addAll(cur);
+            temp.add(nums[i]);
+            dfs(res,visited,nums,temp);
+            visited[i]=false;
+        }
+
+    }
+
 
 }
